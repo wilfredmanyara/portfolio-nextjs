9e68ab94cc7765d136faa93e2e9080828617f76b
wilfredmanyara <manyarawilly46@gmail.com>
2024-12-16 17:06:26 +0300

    feat(portfolio-nextjs): add header & hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
+import { Archivo } from "next/font/google";
 import "./globals.css";
+
+const archivo = Archivo({
+  display: "swap",
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-archivo",
+});
 
 export const metadata: Metadata = {
   title: "Minimal Single Page Portfolio",
-  description: "Created with Frontend Tribe",
+  description: "Created with NextJs",
 };
 
 export default function RootLayout({
@@ -13,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"antialiased"}>{children}</body>
+      <body
+        className={`antialiased bg-stone-200 text-stone-900" ${archivo.variable} font-sans`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
