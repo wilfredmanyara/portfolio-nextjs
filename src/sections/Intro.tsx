42e18631d6ef9217f6afdfe431471ac1a4af60c2
wilfredmanyara <manyarawilly46@gmail.com>
2025-02-07 11:42:52 +0300

    feat(portfolio-nextjs): intro animation

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,8 +1,38 @@
-import { FC } from "react";
+"use client";
+
+import { stagger, useAnimate, useInView } from "motion/react";
+import { FC, useEffect } from "react";
+import SplitType from "split-type";
 
 const Intro: FC = () => {
+  const [scope, animate] = useAnimate();
+  const inView = useInView(scope, {
+    once: true,
+  });
+
+  useEffect(() => {
+    new SplitType(scope.current.querySelector("h2"), {
+      types: "lines,words",
+      tagName: "span",
+    });
+  }, [scope]);
+
+  useEffect(() => {
+    if (inView) {
+      animate(
+        scope.current.querySelectorAll(".word"),
+        {
+          transform: "translateY(0%)",
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.2),
+        }
+      );
+    }
+  }, [inView]);
   return (
-    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro">
+    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro" ref={scope}>
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
           Building beautiful websites with clean code and thoughtful design to
