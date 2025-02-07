95d6114fd956806646a96c1c304aa44ca0af78d5
wilfredmanyara <manyarawilly46@gmail.com>
2025-02-07 15:25:26 +0300

    feat(portfolio-nextjs): FaQs animation

--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -1,4 +1,8 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import { twMerge } from "tailwind-merge";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const faqs = [
@@ -25,32 +29,65 @@ const faqs = [
 ];
 
 const FAQs: FC = () => {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   return (
     <section className="section" id="faqs">
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl">FAQs</h2>
         <div className="mt-10 md:mt-16 lg:mt-20">
-          {faqs.map(({ question, answer }) => (
-            <div key={question} className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py10 last:border-b">
-              <div className="flex items-center justify-between gap-4">
-                <div className="text-2xl md:text-3xl lg:text-4xl">{question}</div>
-                <div className="inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
+          {faqs.map(({ question, answer }, faqIndex) => (
+            <div
+              key={question}
+              className="border-t border-stone-400 border-dotted py-6 md:py-8 
+              lg:py10 last:border-b relative isolate group/faq"
+              onClick={() => {
+                if (faqIndex === selectedIndex) {
+                  setSelectedIndex(null);
+                } else {
+                  setSelectedIndex(faqIndex);
+                }
+              }}
+            >
+              <div className={twMerge("absolute h-0 w-full bottom-0 left-0 bg-stone-300 -z-10 group-hover/faq:h-full transition-all duration-700", faqIndex === selectedIndex && 'h-full')}></div>
+              <div className={twMerge("flex items-center justify-between gap-4 transition-all duration-700 group-hover/faq:lg:px-8", faqIndex === selectedIndex && 'lg:px-8')}>
+                <div className="text-2xl md:text-3xl lg:text-4xl">
+                  {question}
+                </div>
+                <div
+                  className={twMerge(
+                    "inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0 transition duration-300 bg-stone-200",
+                    faqIndex === selectedIndex && "rotate-45"
+                  )}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
                 </div>
               </div>
+              <AnimatePresence>
+                {faqIndex === selectedIndex && (
+                  <motion.div
+                    className="overflow-hidden lg:px-8"
+                    initial={{ height: 0 }}
+                    animate={{ height: "auto" }}
+                    exit={{ height: 0 }}
+                    transition={{ duration: 0.7, ease: "easeOut" }}
+                  >
+                    <p className="text-xl mt-4">{answer}</p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </div>
           ))}
         </div>
